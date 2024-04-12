bb3344005c5dfd5ea4a731aa9e5880a57bcdf283
Lucas Guerra <67044239+vlucasx@users.noreply.github.com>
2024-04-11 22:19:04 -0300

    update automato.py

--- automato.py
+++ automato.py
@@ -1,45 +1,29 @@
-q = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5'}
+from automathon import DFA
+
+Q = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5'}
 sigma = {'L', 'E', 'M', 'C', 'S'}
 
 initial_state = 'q0'
-f = {'q4', 'q5'}
+F = {'q4', 'q5'}
 
-delta = {
-    'q0' : {
-            'M' : {'q1'},
-            'L' : {'q2'}
-            },
-    'q1' : {
-            'M' : {'q1'},
-            'L' : {'q2'}
-            },
-    'q2' : {
-            'M' : {'q1'},
-            'E' : {'q3'}
-            },
-    'q3' : {
-            'E' : {'q3'},
-            'C' : {'q4'},
-            },
-    'q4' : {
-            'S' : {'q5'},
-            }
-}
+delta = { 'q0' : {'L' : 'q2', 'M' : 'q1'},
+          'q1' : {'L' : 'q2',  'M' : 'q1'},
+          'q2' : {'E' : 'q3', 'M' : 'q1'},
+          'q3' : {'E' : 'q3', 'C' : 'q4', },
+          'q4' : {'S' : 'q5'}
+        }
 
-from automathon import NFA
+automata = DFA(Q, sigma, delta, initial_state, F)
+automata.is_valid()
 
-automata = NFA(q, sigma, delta, initial_state, f)
-
-print(automata.is_valid())
-
-print(automata.accept("LEC"))
-print(automata.accept("MLEC"))
-print(automata.accept("MLEC"))
+print(automata.accept("LECS"))
+print(automata.accept("MLEEC"))
+print(automata.accept("LMLMMLEC"))
 print(automata.accept("MEC"))
 print(automata.accept("LM"))
 
 automata.view(
-    file_name="afn_maquina_de_lavar",
+    file_name="afd_maquina_de_lavar",
     node_attr={'fontsize': '20'},
     edge_attr={'fontsize': '20pt'}
 )
