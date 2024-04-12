9c71f02cdb10e9e91b5258b9ccbfded5a04dbd0e
Lucas Guerra <67044239+vlucasx@users.noreply.github.com>
2024-04-11 22:37:27 -0300

    Update automato.py

--- automato.py
+++ automato.py
@@ -1,11 +1,18 @@
 from automathon import DFA
 
+# estados
 Q = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5'}
+
+# alfabeto
 sigma = {'L', 'E', 'M', 'C', 'S'}
 
+# estado inicial
 initial_state = 'q0'
+
+# estado final
 F = {'q4', 'q5'}
 
+# tabela de transição
 delta = { 'q0' : {'L' : 'q2', 'M' : 'q1'},
           'q1' : {'L' : 'q2',  'M' : 'q1'},
           'q2' : {'E' : 'q3', 'M' : 'q1'},
@@ -13,15 +20,20 @@ delta = { 'q0' : {'L' : 'q2', 'M' : 'q1'},
           'q4' : {'S' : 'q5'}
         }
 
+# gera autômato
 automata = DFA(Q, sigma, delta, initial_state, F)
+
+# valida autômato
 automata.is_valid()
 
+# testa palavras no autômato
 print(automata.accept("LECS"))
 print(automata.accept("MLEEC"))
 print(automata.accept("LMLMMLEC"))
 print(automata.accept("MEC"))
 print(automata.accept("LM"))
 
+# visualizar autômato
 automata.view(
     file_name="afd_maquina_de_lavar",
     node_attr={'fontsize': '20'},
